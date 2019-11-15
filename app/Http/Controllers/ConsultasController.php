c16f8c1dcb73c577a4439f5bd46bcd16736f124f
Jhaziel Sarzuri <jhazielsr@gmail.com>
2019-11-14 23:48:11 -0400

    segundo commit

--- app/Http/Controllers/ConsultasController.php
+++ app/Http/Controllers/ConsultasController.php
@@ -21,11 +21,37 @@ class ConsultasController extends Controller
                         ->get();
     }
 
-    public function consulta1 (Request $request) {
+    public function consulta2 () {
+        return Pedidos::join('articulos_pedidos', 'pedidos.id', '=', 'articulos_pedidos.idPedido')
+                        ->join('articulos', 'articulos.id', '=', 'articulos_pedidos.idArticulo')
+                        ->OrderBy('pedidos.fecha', 'asc')
+                        ->get();
+    }
+
+    public function consulta3 (Request $request) {
         return Clientes::join('direcciones', 'direcciones.idCliente', '=', 'clientes.id')
                         ->join('pedidos', 'pedidos.idDireccion', '=', 'direcciones.id')
-                        ->selectRaw('count(*) as Num_pedidos')
+                        ->join('articulos_pedidos', 'pedidos.id', '=', 'articulos_pedidos.idPedido')
+                        ->join('articulos', 'articulos.id', '=', 'articulos_pedidos.idArticulo')
+                        ->select('fecha', 'descripcion as articulo')
+                        ->OrderBy('pedidos.fecha', 'asc')
                         ->where('clientes.nombre', 'like', $request->nombre.'%')
                         ->get();
     }
+
+    public function consulta4 () {
+        return Pedidos::whereRaw('month(`fecha`) >= 1 AND month(`fecha`) <= 3')
+                        ->OrderBy('pedidos.fecha', 'asc')
+                        ->get();
+    }
+
+    public function consulta5 (Request $request) {
+        return Articulos::join('existencias', 'existencias.idArticulo', '=', 'articulos.id')
+                        ->join('fabricas', 'fabricas.id', '=', 'existencias.idFabrica')
+                        ->select('fabricas.id as num_fabrica', 'articulos.id as num_articulo', 'articulos.descripcion')
+                        ->where('fabricas.id', '=', $request->id)
+                        ->get();
+    }
+
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -25,4 +25,14 @@ Route::put('clientes/eliminar/{id}', 'ClientesController@destroy');
 
 //Consultas
 Route::post('consulta/1', 'ConsultasController@consulta1');
-Route::post('consulta/2', 'ConsultasController@consulta2');
+Route::get('consulta/2', 'ConsultasController@consulta2');
+Route::post('consulta/3', 'ConsultasController@consulta3');
+Route::get('consulta/4', 'ConsultasController@consulta4');
+Route::post('consulta/5', 'ConsultasController@consulta5');
+Route::get('consulta/6', 'ConsultasController@consulta6');
+Route::get('consulta/7', 'ConsultasController@consulta7');
+Route::get('consulta/8', 'ConsultasController@consulta8');
+Route::get('consulta/9', 'ConsultasController@consulta9');
+Route::get('consulta/10', 'ConsultasController@consulta10');
+Route::get('consulta/11', 'ConsultasController@consulta11');
+Route::get('consulta/12', 'ConsultasController@consulta12');
